b01d5c60a02d20d450a1903f464e8ef6e6580445
Josh <admin@elmhoe.co.uk>
2017-10-02 20:36:58 +0100

    Slight change

--- src/uk/co/ElmHoe/Bukkit/AntiBow.java
+++ src/uk/co/ElmHoe/Bukkit/AntiBow.java
@@ -142,7 +142,62 @@ public class AntiBow extends JavaPlugin implements Listener {
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("antibow")) {
 			if (!(sender instanceof Player)){
-				if (args.length == 0){
+				switch(args.length){
+				case 0:
+					
+					
+					sender.sendMessage(StringUtility.format("&7&m-----[&4Anti&7-&4Bow&7&m]-----"));
+					sender.sendMessage(StringUtility.format("&6Unfortunately, some commands are in-game only. Access from console is limited for now."));
+					sender.sendMessage(StringUtility.format("&6'/antibow msg <message>' &7&owill change the default message when a player attempts to shoot."));
+					sender.sendMessage(StringUtility.format("&6'/antibow msg-reset' &7&owill set the config message back to the default."));
+					sender.sendMessage(StringUtility.format("&6'/antibow reload' &7&owill reload the configuration file."));
+					sender.sendMessage(StringUtility.format("&7&m-----[&4Anti&7-&4Bow&7&m]-----"));
+					break;
+					
+					
+				case 1:
+					
+					
+					if (args[0].equalsIgnoreCase("msg-reset")) {
+						config.set("DefaultMessages.NotAllowed",
+								"&7[&4Anti&7-&4Bow&7] &6&oSorry %PLAYER%&6&o, but you're not allowed to use the bow in the region: %REGION%");
+						blocked_region = "&7[&4Anti&7-&4Bow&7] &6&oSorry %PLAYER%&6&o, but you're not allowed to use the bow in the region: %REGION%";
+						sender.sendMessage(StringUtility.format("&6Default message has been set."));
+						saveRegion();
+					}else if (args[0].equalsIgnoreCase("reload")){
+						/*
+						 * Reloads the configuration from file.
+						 */
+						loadYamls();
+						buildRegionsList();
+						sender.sendMessage(StringUtility.format("&7[&4Anti&7-&4Bow&7] &6&oHey, I've reloaded the configuration for you."));
+					} else {
+						sender.sendMessage(StringUtility.format("Invalid usage, please check usage by using /antibow"));
+					}
+					break;
+					
+					
+				default:
+					if (args[0].equalsIgnoreCase("msg")) {
+						String newMsg = "";
+						for (int i = 1; i < args.length; i++) {
+							newMsg = newMsg + args[i] + " ";
+						}
+
+					config.set("DefaultMessages.NotAllowed", newMsg);
+					blocked_region = newMsg;
+					sender.sendMessage(StringUtility.format("&6&oSuccess, message has now been set to: " + newMsg));
+					
+					saveRegion();
+					}else{
+						sender.sendMessage(StringUtility.format("&7&m-----[&4Anti&7-&4Bow&7&m]-----"));
+						sender.sendMessage(StringUtility.format("Invalid usage, please check usage by using /antibow"));
+						sender.sendMessage(StringUtility.format("&7&m-----[&4Anti&7-&4Bow&7&m]-----"));
+					}
+					break;
+					
+				}
+/*				if (args.length == 0){
 					sender.sendMessage(StringUtility.format("&7&m-----[&4Anti&7-&4Bow&7&m]-----"));
 					sender.sendMessage(StringUtility.format("&6Unfortunately, some commands are in-game only. Access from console is limited for now."));
 					sender.sendMessage(StringUtility.format("&6'/antibow msg <message>' &7&owill change the default message when a player attempts to shoot."));
@@ -157,9 +212,9 @@ public class AntiBow extends JavaPlugin implements Listener {
 						sender.sendMessage(StringUtility.format("&6Default message has been set."));
 						saveRegion();
 					}else if (args[0].equalsIgnoreCase("reload")){
-						/*
+						
 						 * Reloads the configuration from file.
-						 */
+						 
 						loadYamls();
 						buildRegionsList();
 						sender.sendMessage(StringUtility.format("&7[&4Anti&7-&4Bow&7] &6&oHey, I've reloaded the configuration for you."));
@@ -183,7 +238,7 @@ public class AntiBow extends JavaPlugin implements Listener {
 						sender.sendMessage(StringUtility.format("Invalid usage, please check usage by using /antibow"));
 						sender.sendMessage(StringUtility.format("&7&m-----[&4Anti&7-&4Bow&7&m]-----"));
 					}
-				}
+				}*/
 			}else{
 				Player p = (Player) sender;
 				World ofPlayer = p.getWorld();
